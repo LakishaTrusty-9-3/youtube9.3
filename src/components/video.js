a3a1dd8fe948bcc18122e7fdfc131c1827050f34
Karimah Reavis <Karimah586@gmail.com>
2023-03-14 11:43:05 -0400

    created navBar and attached home and about page

---
+++ src/components/video.js
@@ -0,0 +1,11 @@
+import Youtube from "react-youtube";
+
+export default function ShowVideo(){
+    return (
+    <>
+        <h2>Show Video Page</h2>
+        <Youtube videoId="dpw9EHDh2bM" />
+
+    </>
+);
+}
