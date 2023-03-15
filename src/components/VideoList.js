527ee230767bf9116c3064401b5d0721c50e9501
Karimah Reavis <Karimah586@gmail.com>
2023-03-15 13:36:31 -0400

    more changes

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,12 +1,12 @@
 function VideoList() {
     let {thumbnails, title}= this.props.video.snippet;    
     return(
-        <div id='thumbnail-container'>
-            <div class ='video-container'>
-                <div class='media'>
+        <div className="thumbnail-container">
+            <div className="video-container">
+                <div className="media">
                     <img src={thumbnails.default.url} alt='youtube thumbnail'/>
                 </div>
-                <div class ='content'>
+                <div className="content">
                     <span> {title} </span> 
                     <br/>
                 </div>
